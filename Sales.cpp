7973fa42d87674379b5ce5c3da0e685d1e965a77
tako2039 <takeo2039@gmail.com>
2022-02-28 13:35:20 -0600

    edit header file and add constructors

--- Sales.cpp
+++ Sales.cpp
@@ -8,4 +8,44 @@
 #include <string>
 using namespace std;
 
-/* Write your code here */
+/*~*~*~*~*~*~
+ This is the default constructor; it sets everything to 0 or "".
+ */
+Sales::Sales(){
+    id = "";
+    year = 0;
+    name = "";
+    amountSold = 0;
+}
+
+/*~*~*~*~*~*~
+ These are overloaded constructors.
+ They set the variables according to the parameters.
+*/
+Sales::Sales (string id_val) {
+    id = id_val;
+    year = 0;
+    name = "";
+    amountSold = 0;
+}
+
+Sales::Sales (string id_val, int year_val) {
+    id = id_val;
+    year = year_val;
+    name = "";
+    amountSold = 0;
+}
+
+Sales::Sales (string id_val, int year_val, string name_val) {
+    id = id_val;
+    year = year_val;
+    name = name_val;
+    amountSold = 0;
+}
+
+Sales::Sales (string id_val, int year_val, string name_val, int amountSold_val) {
+    id = id_val;
+    year = year_val;
+    name = name_val;
+    amountSold = amountSold_val;   
+}
--- Sales.h
+++ Sales.h
@@ -12,6 +12,50 @@
 using std::ostream;
 using std::string;
 
-/* Write your code here */
+
+class Sales
+{
+private:
+    string id;
+    int year;
+    string name;
+    int amountSold;
+
+public:
+    // constructors
+    Sales (string id_val, int year_val, string name_val, int amountSold_val);
+    Sales (string id_val, int year_val, string name_val);
+    Sales (string id_val, int year_val);
+    Sales (string id_val);
+    Sales ();
+
+    // setters
+    void setId          (string id_val)          {id = id_val;};
+    void setYear        (int    year_val)        {year = year_val;};
+    void setName        (string name_val)        {name = name_val;};
+    void setAmountSold  (int    amountSold_val)  {amountSold = amountSold_val;};
+    
+    // getters
+    string  getId()     const {return id;};
+    int     getYear()   const {return year;};
+    string  getName()   const {return name;};
+    int     getAmount() const {return amountSold;};
+
+    // other functions
+    void    display () const;
+    double  detCommissionPercent (int amountSold) const;
+    double  detAmountEarned (int amountSold) const;
+
+    // overloaded operators
+    bool operator < (const Sales right) const;
+    bool operator == (const Sales right) const;
+    bool operator > (const Sales right) const;
+    bool operator >= (const Sales right) const;
+    bool operator <= (const Sales right) const;
+    bool operator != (const Sales right) const;
+
+    // friends
+    friend ostream &operator << (ostream &strm, Sales &obj); 
+};
 
 #endif
